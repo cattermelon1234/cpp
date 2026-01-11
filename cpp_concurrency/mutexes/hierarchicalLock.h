7f0a645a8cab37ceae93b7fd03f3ccb96fd428f4
cattermelon1234 <74235189+cattermelon1234@users.noreply.github.com>
2026-01-10 17:36:03 -0800

    fix to hierarchy value

--- cpp_concurrency/mutexes/hierarchicalLock.h
+++ cpp_concurrency/mutexes/hierarchicalLock.h
@@ -1,15 +1,16 @@
 #include <mutex>
+#include <stdexcept>
+#include <climits>
 
 class hierarchical_mutex {
-private:
   std::mutex m_;
   unsigned long const hierarchy_value;
   unsigned long prev_hierarchy_value;
-  static thread_local unsigned long cur_thread_hierarchy_value;
+  static inline thread_local unsigned long this_thread_hierarchy_value = ULONG_MAX;
 
   void check_for_hierarchy_violation() {
     // if a thread with a lower hierarchy value tries to acquire our mutex, block
-    if (cur_thread_hierarchy_value <= hierarchy_value) {
+    if (this_thread_hierarchy_value <= hierarchy_value) {
       throw std::logic_error("cannot acquire mutex of higher hierarchy value!"); 
     }
   } 
@@ -21,14 +22,14 @@ public:
     check_for_hierarchy_violation();
     m_.lock();
     // update thread hierarchy value
-    prev_hierarchy_value = cur_thread_hierarchy_value;
-    cur_thread_hierarchy_value = hierarchy_value;
+    prev_hierarchy_value = this_thread_hierarchy_value;
+    this_thread_hierarchy_value = hierarchy_value;
   }
   void unlock() {
-    if (cur_thread_hierarchy_value != hierarchy_value) {
+    if (this_thread_hierarchy_value != hierarchy_value) {
       throw std::logic_error("cannot unlock mutex of higher hierarchy without unlocking lower mutex first!");
     }
-    cur_thread_hierarchy_value = prev_hierarchy_value;
+    this_thread_hierarchy_value = prev_hierarchy_value;
     m_.unlock();
   }
   bool try_lock() {
@@ -36,8 +37,8 @@ public:
     if (!m_.try_lock()) {
       return false;
     }
-    prev_hierarchy_value = cur_thread_hierarchy_value;
-    cur_thread_hierarchy_value = hierarchy_value;
+    prev_hierarchy_value = this_thread_hierarchy_value;
+    this_thread_hierarchy_value = hierarchy_value;
     return true;
   }
 };
